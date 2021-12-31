f2555fbf6d5b150c77eee12c6f78693b833ce7a2
Faith Tiong <57514875+xytfaith@users.noreply.github.com>
2022-01-01 09:13:06 +1100

    Intro.py

---
+++ Intro.py
@@ -0,0 +1,18 @@
+#Code below
+#My personal intro v2
+#just for fun haha :)
+
+print("My name is Faith, just a lil noob and newbie in the big as IT world lol, trying my best to learn more and study more in my field, especially for Cyber Security.")
+
+while True:
+    you_like_my_profile = input("Enter Yes/Nay only\n")
+    if you_like_my_profile == "Yes":
+        print("Then feel free to follow, lots of love!!")
+        break
+    elif you_like_my_profile == "Nay":
+        print("No worries, I will try my best to improve too, feel free to comment below too!!")
+        break
+    else:
+        print("As I said before, Yes/Nay only, please try again")
+    
+Thank you so much for viewing :)
